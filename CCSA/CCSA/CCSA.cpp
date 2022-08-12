d493dabe88e7f1b95514ff99e5f27a66767e6aa6
Gabriel Jones <gabrieljones900@yahoo.com>
2022-08-12 16:21:44 -0700

    Fixed Bug
    used Double instead of Int to take care of triangle area bug

--- CCSA/CCSA/CCSA.cpp
+++ CCSA/CCSA/CCSA.cpp
@@ -13,7 +13,7 @@ public:
 
     string Color;
     
-    int getArea(int x, int y)
+    int getArea(double x, double y)
     {
         int area = x * y;
         cout << "The Area Is: " << area << endl;
@@ -27,8 +27,8 @@ public:
 class Rectangle : public Shape {
 public:
 
-    int height;
-    int width;
+    double height;
+    double width;
 
 };
 
@@ -36,15 +36,15 @@ public:
 class Triangle : public Shape {
 public:
 
-    int height;
-    int base;
+    double height;
+    double base;
 
 };
 // Rectangle sub class derived from a single Shape base class
 class Circle : public Shape {
 public:
 
-    int radius;
+    double radius;
     
 
 };
@@ -57,8 +57,8 @@ int main()
     Rectangle rect1;
 
     rect1.Color = "green";
-    rect1.height = 5;
-    rect1.width = 3;
+    rect1.height = 5.0;
+    rect1.width = 3.0;
     cout << "Rectangle Data:\n";
     cout << "Color: " << rect1.Color << "\n";
     cout << "Height: " << rect1.height << "\n";
@@ -71,13 +71,13 @@ int main()
     Triangle tri1;
 
     tri1.Color = "blue";
-    tri1.height = 5;
-    tri1.base = 4;
+    tri1.height = 5.0;
+    tri1.base = 4.0;
     cout << "Triangle Data:\n";
     cout << "Color: " << tri1.Color << "\n";
     cout << "Height: " << tri1.height << "\n";
     cout << "Base: " << tri1.base << "\n";
-    rect1.getArea(tri1.height * tri1.base, .5);
+    rect1.getArea(tri1.height * tri1.base, 0.5);
     cout << "\n";
 
    // Creating object of Circle sub class will
@@ -85,7 +85,7 @@ int main()
     Circle cir1;
 
     cir1.Color = "red";
-    cir1.radius = 2;
+    cir1.radius = 2.0;
     
     cout << "Rectangle Data:\n";
     cout << "Color: " << cir1.Color << "\n";
